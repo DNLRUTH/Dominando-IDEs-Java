e9d6e891d4309df4748e9f3ce6ef6d061439d356
danilo <danilo.ruth@hotmail.com>
2021-10-31 22:39:20 -0300

    first commit

---
+++ src/aulaide/aulaide1.java
@@ -0,0 +1,15 @@
+package aulaide;
+
+import aulaide.ide.ide;
+
+public class aulaide1 {
+public static void main(String args[]) {
+		ide ide = new ide(null, null, null);
+		livros livros = new livros ();
+		
+		System.out.println(ide);
+		System.out.println(livros);	}
+
+}
+
+class livros {}
